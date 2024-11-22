a5b787c257285e78cb9c8f1b8e4b993c99e09e5f
Матьё Роар <alapetite.mathieu@gmail.com>
2024-11-22 15:10:44 -0500

    10.9 - form validation

--- src/components/Login/SignIn.jsx
+++ src/components/Login/SignIn.jsx
@@ -1,7 +1,20 @@
 import { Pressable, View, TextInput, StyleSheet } from 'react-native';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 import Text from '../Text';
 import theme from '../../theme';
+
+
+const validationSchema = yup.object().shape({
+	name: yup
+		.string()
+		.test('len', 'Username must be greater than 5', (val) => val.length > 4)
+		.required('You must enter your username.'),
+	password: yup
+		.string()
+		.test('len', 'Password must be greater than 8', (val) => val.length > 7)
+		.required('You must enter a password.'),
+})
 
 const onSubmit = (values) => {
 	console.log('function onSubmit:', values);
@@ -13,24 +26,39 @@ const SignInForm = () => {
 			name: '',
 			password: '',
 		},
-		onSubmit: (values) => { console.log(values) }
+		validationSchema,
+		onSubmit,
 	});
+
 
 	return (
 		<View style={styles.form}>
 			<TextInput
-				style={styles.input}
+				style={[
+					styles.input,
+					(formik.errors.name && formik.touched.name) && styles.error
+				]}
 				placeholder='Username'
 				value={formik.values.name}
 				onChangeText={formik.handleChange('name')}
 			/>
+			{formik.touched.name && formik.errors.name && (
+				<Text style={styles.error}>{formik.errors.name}</Text>
+			)}
 			<TextInput
-				style={styles.input}
+				style={[
+					styles.input,
+					(formik.errors.password && formik.touched.password) && styles.error
+				]
+				}
 				secureTextEntry
 				placeholder='Password'
 				value={formik.values.password}
 				onChangeText={formik.handleChange('password')}
 			/>
+			{formik.touched.password && formik.errors.password && (
+				<Text style={styles.error}>{formik.errors.password}</Text>
+			)}
 			<Pressable
 				onPress={formik.handleSubmit} >
 				<Text style={styles.button}>Sign in</Text>
@@ -75,6 +103,11 @@ const styles = StyleSheet.create({
 		borderRadius: 5,
 		textAlign: 'center',
 		verticalAlign: 'middle',
+	},
+	error: {
+		color: '#d73a4a',
+		borderColor: '#d73a4a',
+
 	}
 })
 
